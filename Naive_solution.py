b8642da1a2fad684e40ae8b8708d68974eedf677
J Lian <lianjin369@hotmail.com>
2018-03-14 16:50:53 +0000

    Implemted SVM, support vector is stored in alpha_store.npy file

--- Naive_solution.py
+++ Naive_solution.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy
+#import matplotlib.pyplot as plt
+import cvxopt
 #import matlibplot
 
 
@@ -11,6 +13,40 @@ def GDSolution(X_in, Y_in):
     lossFunction(X_in, Y_in, w);
 
     return w
+
+def kBuilder(X_in, Y_in):
+    '''
+    Compute quadractic coefficient for quadractic programming to find linear constant
+    '''
+    '''
+    K = np.zeros(shape=(X_in.shape[0],0));
+    X_in_trans = np.transpose(X_in);
+    for i in range(X_in.shape[0]):
+        temp = np.matmul(X_in, X_in[i,:].transpose());
+        K = np.insert(K, K.shape[1], temp.transpose(), axis=1);
+        if (i% X_in.shape[0] / 10 == 0):
+            print i,np.shape(K),np.shape(temp)
+    '''
+
+    X_mult = np.matmul(X_in, X_in.transpose());
+    Y_mult = np.matmul(Y_in, Y_in.transpose());
+    print np.shape(Y_mult), np.shape(X_mult);
+    K = np.multiply(X_mult, Y_mult);
+    return K
+
+def quadracticProgramming(X_in, Y_in):
+    no_of_samples = X_in.shape[0];
+    K = cvxopt.matrix(kBuilder(X_in, Y_in));
+    vector_neg_ones = cvxopt.matrix(-np.ones(shape=(no_of_samples,1)));
+    G = cvxopt.matrix(-np.eye(no_of_samples));
+    vector_zeros = cvxopt.matrix(np.zeros(no_of_samples));
+    Y_in_trans = cvxopt.matrix(Y_in.transpose());
+    b = cvxopt.matrix(np.zeros(shape=(1)));
+    cvxopt.solvers.options['show_progress'] = False;
+    sol = cvxopt.solvers.qp(K, vector_neg_ones, G, vector_zeros, Y_in_trans, b);
+    alphas = np.array(sol['x']);
+    return alphas
+
 
 def ZGeneration(X_in):
     Z = X_in;
@@ -62,8 +98,19 @@ def main():
         Y_in = raw_data[:,raw_data.shape[1]-1];
 
     # Z_in = ZGeneration(X_in);
+    Y_in = np.reshape(Y_in, (4601,1));
     ### using naive solution to solve linear regression
+    alpha = quadracticProgramming(X_in,Y_in);
+    np.save('alpha_store.npy', alpha);
+    print alpha
+    alpha_avg = np.average(alpha);
+    alpha_effective = alpha[(alpha>alpha_avg).nonzero()];
+    print alpha_effective, np.shape(alpha_effective);
+
+    '''
     w = naiveSolution(X_in, Y_in);
+
+
     Y_out = np.matmul(X_in, w);
     if using_overall:
         Y_final_out = np.round(Y_out);
@@ -74,10 +121,7 @@ def main():
     correct_no_output = np.sum(error);
     error_rate = 1 - float(correct_no_output)/np.shape(error)[0]
     print error_rate
-    
-def pseudoInverse(X):
-    X_size = np.shape(X_in_rand);
-    
+    '''
 
 if __name__ == "__main__":
     main();
